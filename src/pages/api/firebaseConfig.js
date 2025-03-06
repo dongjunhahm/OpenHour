8af784346bdc0f1cda9434fb46c06e231d03eba6
Dongjun Hahm <dhahm@ucsc.edu>
2025-03-05 18:46:18 -0800

    adding firebase setup and folder structure

---
+++ src/pages/api/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FB_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FB_APPID,
+}
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
