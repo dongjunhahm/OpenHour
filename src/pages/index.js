7bad38d25a80cda3dd6277693e2740d2922922a2
Dongjun Hahm <dhahm@ucsc.edu>
2025-03-06 01:56:01 -0800

    added framework for login, finished configuring firebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,16 @@
 "use client";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged, signOut} from "firebase/auth";
+import "../styles/globals.css";
+
+const Home = () => {
+
+  return (
+    <div>
+      <button>Hi world</button>
+    </div>
+  );
+};
+
+export default Home;
