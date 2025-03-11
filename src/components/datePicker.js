b4ce931239691e4b22697566dc2fe5523044b2d1
Dongjun Hahm <dhahm@ucsc.edu>
2025-03-10 23:43:26 -0700

    working now on letting users select dates, updated event fetching with calendar api to only use the range the user selects

---
+++ src/components/datePicker.js
@@ -0,0 +1,61 @@
+import "cally";
+import { useState } from "react";
+
+const DatePicker = () => {
+  const [dateRange, setDateRange] = useState("");
+
+  const handleDateChange = (event) => {
+    const selectedRange = event.target.value; // formatted in yyyy-mm-dd/yyyy-mm-dd
+    setDateRange(selectedRange);
+  };
+
+  return (
+    <div>
+      <button
+        popoverTarget="cally-popover1"
+        className="input input-border"
+        id="cally1"
+        style={{ positionAnchor: "--cally1" }} // Corrected the style prop
+      >
+        Pick a date
+      </button>
+      <div
+        popover
+        id="cally-popover1"
+        className="dropdown bg-base-100 rounded-box shadow-lg"
+        style={{ positionAnchor: "--cally1" }}
+      >
+        <calendar-range
+          class="cally"
+          value={dateRange}
+          min="2024-01-01"
+          max="2024-12-31"
+          locale="en-GB"
+          onchange={handleDateChange} // Update the date range on change
+        >
+          <svg
+            aria-label="Previous"
+            className="fill-current size-4"
+            slot="previous"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M15.75 19.5 8.25 12l7.5-7.5"></path>
+          </svg>
+          <svg
+            aria-label="Next"
+            className="fill-current size-4"
+            slot="next"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
+          </svg>
+          <calendar-month></calendar-month>
+        </calendar-range>
+      </div>
+    </div>
+  );
+};
+
+export default DatePicker;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,13 +3,38 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import React, { useState, useEffect } from "react";
+import EventsList from "../components/eventsList";
+import DatePicker from "../components/datePicker";
 
 const Dashboard = () => {
   const router = useRouter();
   const token = useSelector((state) => state.token.token);
+  const [dateRange, setDateRange] = useState("");
+  const [events, setEvents] = useState([]);
+
+  const handleDateChange = (event) => {
+    const selectedRange = event.target.value;
+    setDateRange(selectedRange);
+  };
+
+  const handleFetchEvents = async () => {
+    const [startDate, endDate] = dateRange.split("/");
+
+    try {
+      const fetchedEvents = await axios.post("/api/read-events", {
+        token: token,
+        startDate: startDate,
+        endDate: endDate,
+      });
+      setEvents(fetchedEvents);
+    } catch (error) {
+      console.error("error fetching events", error);
+    }
+  };
 
   useEffect(() => {
     console.log("Token in dashboard:", token);
+    const fetchEvents = async () => {};
   }, [token]);
 
   const createSharedCalendar = async () => {
@@ -31,16 +56,19 @@ const Dashboard = () => {
 
   return (
     <div>
-      <button
-        className="btn btn-ghost btn-neutral"
-        onClick={createSharedCalendar}
-      ></button>
+      <button className="btn btn-success" onClick={createSharedCalendar}>
+        Create Shared Calendar
+      </button>
       <button
         className="btn btn-ghost btn-neutral"
         onClick={console.log(token)}
       >
         check token
       </button>
+      <div>
+        <EventsList events={events}></EventsList>
+      </div>
+      <DatePicker></DatePicker>
     </div>
   );
 };
