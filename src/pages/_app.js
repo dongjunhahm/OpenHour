0bdad6ccacf7430cc411d6332fb234ff04942a31
Dongjun Hahm <dhahm@ucsc.edu>
2025-04-11 22:10:59 -0700

    fixed globals css not applying to all pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import { Provider } from "react-redux";
 import store from "../store/store"; // Import your store
+import "../styles/globals.css"; // Import global CSS
 
 function MyApp({ Component, pageProps }) {
   return (
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,7 +1,6 @@
 "use client";
 import Navbar from "../components/navbar";
 import { Footer } from "../components/footer";
-import "../styles/globals.css";
 
 const Landing = () => {
   return (
