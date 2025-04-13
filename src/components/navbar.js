36114f785c17ece033f87c597580a321ea504ff1
Johann1ee <johannlee03@gmail.com>
2025-04-13 02:22:23 -0700

    removed sidebar, respaced dashboard

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { clearToken } from "../store/tokenSlice";
 
-const Navbar = ({ toggleSidebar }) => {
+const Navbar = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token.token);
@@ -24,22 +24,6 @@ const Navbar = ({ toggleSidebar }) => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost" onClick={toggleSidebar}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current"
-          >
-            {" "}
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>{" "}
-          </svg>
-        </button>
       </div>
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">OpenHour</a>
